1b551a2815cc66786b864e57e8e19571a2d05331
David Norton <dgnorton@quoininc.com>
2014-03-26 21:59:15 -0400

    Added devbuild.bash.

--- devbuild.bash
+++ devbuild.bash
@@ -3,8 +3,11 @@
 while [ 1 ]
 do
    clear
-   echo "building..."
-   go build
+   echo "building mesh..."
+   go build mesh.go
+   echo ""
+   echo "building meshTest..."
+   go build meshTest.go
    echo "done."
    sleep 4
 done
